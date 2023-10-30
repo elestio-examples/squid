464a10271f0a881d6c6399733b971967a6d51c9b
Prem chavhan <pchavhan@logiquad.com>
2023-10-30 18:39:04 +0530

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# cp -rf ./packages/hoppscotch-backend/* ./
-docker buildx build . --target backend --output type=docker,name=elestio4test/squid:latest | docker load
+sed -i "s~ARG SQUID_VER=6.4~ARG SQUID_VER=6.3~g" Dockerfile
+docker buildx build . --output type=docker,name=elestio4test/squid:latest | docker load
